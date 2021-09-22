324a371882f44538deed519a81ac13e33a27e5bf
samrobers <sam.roberson32@gmail.com>
2021-09-22 17:09:29 -0600

    working on state for search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 function Search() {
-    return (
-        <div>
-            
-        </div>
-    )
+  let [formState, setFormState] = useState({ search: "" });
+
+  const handleChange = (event) => {
+    ("handle Change---");
+    setFormState(event.target.value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log("handleFormSubmit---");
+  };
+  return (
+    <>
+      <form
+        className="navbar-form d-flex justify-content-center"
+        onSubmit={handleFormSubmit}
+      >
+        <input type="text" className="form-control w-25" placeholder="Search" />
+        <button type="submit" className="btn btn-primary btn-lg">
+          Search
+        </button>
+      </form>
+    </>
+  );
 }
 
-export default Search
+export default Search;
